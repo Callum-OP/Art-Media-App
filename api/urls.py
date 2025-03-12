9c9a3e09311d56b320f907e369e732626ae2d875
Callum-OP <callumoprey@gmail.com>
2025-03-12 20:30:42 +0000

    Added url and html for viewing images from a specific post

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,12 @@ from .views import PostList
 from .views import CommentList
 from .views import SpecificPost
 from .views import SpecificComment
+from .views import Image
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', SpecificPost.as_view(), name='post'),
     path('posts/<int:fk>/comments/<int:pk>/', SpecificComment.as_view(), name='comment'),
     path('comments/', CommentList.as_view(), name='comment-list'),
-    #path('images/', Picture.as_view(), name='picture'),
+    path('posts/<int:pk>/image/', Image.as_view(), name='image'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render
 from .models import Post
 from .models import Comment
 from .serializers import PostSerializer
@@ -19,6 +20,12 @@ class PostList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response({"error": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Image(APIView):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        return render(request, 'image.html', {'post': post})
 
 
 class SpecificPost(APIView):
