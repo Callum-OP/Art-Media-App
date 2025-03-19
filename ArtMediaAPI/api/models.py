5067463cb684c9a340210121cf019a80dc4f4189
Callum-OP <callumoprey@gmail.com>
2025-03-19 16:41:26 +0000

    Made id for models unique by using uuid

--- ArtMediaAPI/api/models.py
+++ ArtMediaAPI/api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import django.utils.timezone
+import uuid
 
 class Post(models.Model):
-    # Stores username, image, body of text and time created
+    # Stores unique id, username, image, body of text and time created
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255)
     image = models.ImageField(upload_to='uploads/', blank=True) # Image is optional
     text = models.TextField()
@@ -12,7 +14,8 @@ class Post(models.Model):
         return self.name
 
 class Comment(models.Model):
-    # Stores post ID, username, body of text and time created
+    # Stores unique id, post ID, username, body of text and time created
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE) # Ensure when post is deleted, comments are also deleted
     username = models.CharField(max_length=255)
     text = models.TextField()
--- ArtMediaAPI/api/urls.py
+++ ArtMediaAPI/api/urls.py
@@ -11,9 +11,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'), # Creating a post and viewing all posts
-    path('posts/<int:pk>/', SpecificPost.as_view(), name='post'), # Viewing, editng and deleting a post
-    path('posts/<int:fk>/comments/<int:pk>/', SpecificComment.as_view(), name='comment'), # Viewing, editng and deleting a comment
-    path('posts/<int:pk>/image/', Image.as_view(), name='image'), # Viewing an image
+    path('posts/<str:pk>/', SpecificPost.as_view(), name='post'), # Viewing, editng and deleting a post
+    path('posts/<str:fk>/comments/<str:pk>/', SpecificComment.as_view(), name='comment'), # Viewing, editng and deleting a comment
+    path('posts/<str:pk>/image/', Image.as_view(), name='image'), # Viewing an image
     path('comments/', CommentList.as_view(), name='comment-list'), # Creating a comment
     path('users/', RegisterUser.as_view(), name='register-user'), # Creating a user account
     path('token/', GetToken.as_view(), name='get_token'),
