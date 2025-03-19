a83b3af9d8af955f456c125f998b592756038baa
Callum-OP <callumoprey@gmail.com>
2025-03-19 17:01:07 +0000

    Added option for users to add video files as well as images

--- ArtMediaAPI/api/models.py
+++ ArtMediaAPI/api/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 import django.utils.timezone
 import uuid
+from django.core.exceptions import ValidationError
+
+def validate_file(value):
+    # Check if a file matches the extensions allowed
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.mp4', '.mov', '.avi', '.mkv', '.gif']
+    import os
+    ext = os.path.splitext(value.name)[1]
+    if ext.lower() not in valid_extensions:
+        raise ValidationError(f'{ext} file type is unsupported: {ext}. Only image and video files are allowed here.')
+
 
 class Post(models.Model):
     # Stores unique id, username, image, body of text and time created
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='uploads/', blank=True) # Image is optional
+    image = models.FileField(upload_to='uploads/', blank=True, validators=[validate_file]) # Image/video is optional
     text = models.TextField()
     created_at = models.DateTimeField(default=django.utils.timezone.now)
 
     def __str__(self):
         return self.name
+
 
 class Comment(models.Model):
     # Stores unique id, post ID, username, body of text and time created
